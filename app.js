b8d7b6bc9c782f00ac4c49d93945ab07c38e8c6a
Nikhil Kumar <nnnnikhil6@gmail.com>
2023-12-30 22:14:32 +0530

    adding cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
@@ -8,6 +9,10 @@ dotenv.config({ path: "./config.env" });
 require("./db/mongoConn.js");
 
 app.use(express.json());
+npm.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
 app.use(require("./router/auth.js"));
 
 const PORT = process.env.PORT;
